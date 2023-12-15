4c85ecd42c90b4dcc851960236710d9d31be4bc1
Clieford13 <143694403+Clieford13@users.noreply.github.com>
2023-12-16 01:05:15 +0800

    Abstract_Mango.java

---
+++ Abstract/Abstract_Mango.java
@@ -0,0 +1,9 @@
+package Abstract;
+
+public class Abstract_Mango extends Abstract_Fruits {
+    
+    public void sustansyado(){
+        System.out.println(" pinakaganahan nako ");
+        System.out.println(" lami kaayo kini labi nag tam-is ");
+    }
+}
