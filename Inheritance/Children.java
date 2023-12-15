60ce2afb6858262568b011d1823fcdf9698b93cb
Clieford13 <cliefordjayrule@gmail.com>
2023-12-16 02:14:05 +0800

    Inheritance

---
+++ Inheritance/Children.java
@@ -0,0 +1,12 @@
+package Inheritance;
+public class Children extends Parent {
+            Children(String name){
+        super(name);
+    }
+        void playgames(){
+        System.out.println(name + " kay gadula ug ML labi nag wala nay buhaton. ");
+    } 
+        void talk(){
+        System.out.println(name + " kay sabaan kaayo kapag close niya ang iyang makauban. ");
+    }
+}
---
+++ Inheritance/Main.java
@@ -0,0 +1,10 @@
+package Inheritance;
+public class Main {
+        public static void main(String[] args){
+            Children myChild = new Children (" Cleiford Jay Rule "); 
+        myChild.playgames();
+        myChild.eat();
+        myChild.talk();
+        myChild.sleep();
+    }
+}
