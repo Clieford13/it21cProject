5fa39e1ce3fa3a89509b0ad8ced96815b3bb803e
Clieford13 <cliefordjayrule@gmail.com>
2023-12-16 00:57:56 +0800

    Abstract

---
+++ Abstract/Abstract_Apple.java
@@ -0,0 +1,9 @@
+package Abstract;
+
+public class Abstract-Apple extends Abstract-Fruits{
+    
+    public void sustansyado(){
+        System.out.println(" makapalit ra kag bisan 10 ");
+        System.out.println(" galagorot pag kaunon ");
+    }
+}
---
+++ Abstract/Main.java
@@ -0,0 +1,16 @@
+package Abstract;
+
+public class Main {
+    public static void main(String[] args){
+        
+      Abstract_Fruits my_fruits = new Fruits() {
+         my_Fruits.tamis ();
+         my_Fruits.mapalit ();
+         my_Fruits.lami ();
+      Abstract_Apple my_apple = new Apple() {
+        my_apple.barato();
+        my_apple.lagorot();
+      Abstract_Mango my_mango = new Mango() {    
+         my_mango.sustansyado();
+    }
+}
