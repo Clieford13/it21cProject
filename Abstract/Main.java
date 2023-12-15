902a98cf215937447f0277c2807998b0d164d418
Clieford13 <143694403+Clieford13@users.noreply.github.com>
2023-12-16 01:09:06 +0800

    Main.java

--- Abstract/Main.java
+++ Abstract/Main.java
@@ -1,16 +1,15 @@
-package Abstract;
-
-public class Main {
-    public static void main(String[] args){
+public class Main{
+    public static void main(String[] args) {
         
       Abstract_Fruits my_fruits = new Fruits() {
-         my_Fruits.tamis ();
-         my_Fruits.mapalit ();
-         my_Fruits.lami ();
-      Abstract_Apple my_apple = new Apple() {
+         my_fruits.tamis();
+         my_fruits.mapalit();
+         my_fruits.lami();
+      Abstract_Apple my_apple = new Apple();
         my_apple.barato();
         my_apple.lagorot();
-      Abstract_Mango my_mango = new Mango() {    
+      Abstract_Mango my_mango = new Mango();
          my_mango.sustansyado();
+      }
     }
 }
