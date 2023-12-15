e95c90ccb9c78ae4743cb9bee1b2cd4106827bbf
Clieford13 <cliefordjayrule@gmail.com>
2023-12-16 01:59:42 +0800

    BankAccount

---
+++ BankAccount/Main.java
@@ -0,0 +1,12 @@
+package BankAccount;
+public class Main {
+            public static void main(String[] args) {
+        BankAccount  a=new BankAccount();
+    a.setName("Evarine Rayon");
+    a.setAge(24);
+    a.setAccountNumber(20221086);
+    a.deposit(30000.0);
+    a.deposit(90000.0);
+    a.displaySummary();
+    }
+}
